725fee706907f67607e05c85d882d0c5ab930826
MashaK04 <masha-kashirina2016@yandex.ru>
2025-02-28 22:14:14 +0300

    Добавление отчетов + автотест

--- pages/form_page.py
+++ pages/form_page.py
@@ -6,29 +6,28 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoAlertPresentException
 
 
-
 class FormPage:
     def __init__(self, driver):
         self.driver = driver
 
-    #Получение имени
+    # Получение имени
     def fill_name(self, name):
-        name_input = self.driver.find_element(By.ID, "name-input")
+        name_input = self.driver.find_element(By.CSS_SELECTOR, "#name-input")
         name_input.send_keys(name)
 
-    #Получение пароля
+    # Получение пароля
     def fill_password(self, password):
         password_input = self.driver.find_element(By.XPATH, "//input[@type='password']")
         password_input.send_keys(password)
 
-    #Выбор любимого напитка
+    # Выбор любимого напитка
     def select_favorite_drinks(self):
         milk_checkbox = self.driver.find_element(By.ID, "drink2")
         coffee_checkbox = self.driver.find_element(By.ID, "drink3")
         milk_checkbox.click()
         coffee_checkbox.click()
 
-    #Выбор любимого цвета
+    # Выбор любимого цвета
     def select_favorite_color(self):
         try:
             yellow_radio = WebDriverWait(self.driver, 10).until(
@@ -39,7 +38,7 @@ class FormPage:
         except Exception as e:
             print(f"Ошибка при выборе цвета: {e}")
 
-    #Выбор случайного варианта
+    # Выбор случайного варианта
     def select_random_automation_option(self):
         select_element = Select(self.driver.find_element(By.ID, "automation"))
         options = select_element.options
@@ -48,35 +47,36 @@ class FormPage:
             random_option = random.choice(valid_options)
             select_element.select_by_visible_text(random_option.text)
 
-    #Получение поля
+    # Получение поля
     def fill_email(self, email):
         email_input = self.driver.find_element(By.ID, "email")
         email_input.send_keys(email)
 
-    #Ввод сообщения
+    # Ввод сообщения
     def fill_message(self, tools):
         message_input = self.driver.find_element(By.ID, "message")
         message_text = f"{len(tools)} tools: {max(tools, key=len)}"
         message_input.send_keys(message_text)
 
-    #Сабмит
+    # Сабмит
     def submit_form(self):
         try:
             submit_button = self.driver.find_element(By.ID, "submit-btn")
-            self.driver.execute_script("arguments[0].click();",  submit_button)
-            submit_button.click()
+            self.driver.execute_script("arguments[0].click();", submit_button)
 
             print("Форма успешно отправлена.")
         except Exception as e:
             print(f"Ошибка при отправке формы: {e}")
 
-
     def check_alert_message(self):
         try:
-            alert_obj = self.driver.switch_to.alert
-            msg = alert_obj.text()
-            print(msg)
+            alert_obj = WebDriverWait(self.driver, 10).until(EC.alert_is_present())
+            msg = alert_obj.text
+            print("Полученный алерт:",msg)
+            assert msg == "Message received!", "Сообщение не совпадает с ожидаемым!"
+            alert_obj.accept()
+        except NoAlertPresentException:
+            print("Алерт не присутствует.")
         except Exception as e:
             print(f"Ошибка при проверке текста алерта: {e}")
-
 
---
+++ tests/read_result.py
@@ -0,0 +1,69 @@
+
+import pendulum
+import glob
+import json
+import os
+
+# Поиск нужного json файла с результатами
+pattern = '../allure-results/*-result.json'
+result_files = glob.glob(pattern)
+
+if not result_files:
+    print("Нет найденных файлов.")
+else:
+    # Поиск нового файла
+    latest_file = max(result_files, key=os.path.getmtime)
+
+
+    with open(latest_file, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    #Преобразование данных в текстовый формат
+    #Пробразование дат из Unix в московское время
+    start_data = data['start']
+    end_data = data['stop']
+    #Начало текста в московское время
+    utc_time_start = pendulum.from_timestamp(start_data / 1000, tz='UTC')
+    moscow_time_start = utc_time_start.in_tz('Europe/Moscow')
+    #Окончание
+    utc_time_end = pendulum.from_timestamp(start_data / 1000, tz='UTC')
+    moscow_time_end = utc_time_end .in_tz('Europe/Moscow')
+
+
+
+    result_text = f"""
+    Тестирование отправки формы для практикума
+    ---------------------------------------------
+    Статус: {data.get('status', 'неизвестно')}
+    Описание: Тест проверяет функциональность отправки формы на сайте practice-automation.com
+        
+    Время начала: {moscow_time_start}
+    Время окончания: {moscow_time_end}
+    UUID: {data['uuid']}
+    History ID: {data['historyId']}
+    Test Case ID: {data['testCaseId']}
+    Полное имя: {data['fullName']}
+    Шаги теста: 
+    """
+    for step in data.get('steps', []):
+        start_data = data['start']
+        end_data = data['stop']
+        # Начало текста в московское время
+        utc_time_start = pendulum.from_timestamp(start_data / 1000, tz='UTC')
+        moscow_time_start = utc_time_start.in_tz('Europe/Moscow')
+        # Окончание
+        utc_time_end = pendulum.from_timestamp(start_data / 1000, tz='UTC')
+        moscow_time_end = utc_time_end.in_tz('Europe/Moscow')
+        result_text += f"- {step['name']}: {step['status']} (начало: {moscow_time_start}, конец: {moscow_time_end})\n"
+
+
+    if data.get('attachments'):
+        result_text += "\nПриложения:\n"
+        for attachment in data['attachments']:
+            result_text += f"- {attachment['name']} (тип: {attachment['type']}, источник: {attachment['source']})\n"
+
+    # Сохранение данных в текстовый файл
+    with open('Результаты_теста.txt', 'w', encoding='utf-8') as file:
+            file.write(result_text.strip())
+
+    print("Отчет создан!")
